588041fc7ede7890c678fc34e965ae38531f30d9
Konrad Uciechowski <konraduciechowski@MacBook-Pro-Konrad-2.local>
2019-11-01 16:01:29 +0100

    create proxy over tor

---
+++ proxy/run_tor_proxy.py
@@ -0,0 +1,5 @@
+from utils.proxy.tor_proxy import TorProxy
+
+tor = TorProxy()
+
+tor.init_proxy()
--- utils/proxy/tor_proxy.py
+++ utils/proxy/tor_proxy.py
@@ -1,9 +1,15 @@
+import subprocess
+
+from torpy.http.requests import TorRequests
+
 from utils.proxy.proxy import Proxy
 
 
 class TorProxy(Proxy):
     def init_proxy(self):
-        pass
+        self.run_proxy()
 
     def run_proxy(self):
-        pass
+        tor = subprocess.run(["tor"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(tor)
+        print(subprocess.PIPE)
